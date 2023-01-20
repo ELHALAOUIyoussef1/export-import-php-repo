4282d11aab8348a21b030dc1577d0ad2ee6f4511
ELHALAOUIyoussef1 <108194581+ELHALAOUIyoussef1@users.noreply.github.com>
2023-01-20 17:13:39 +0100

    update export.php

--- php/export.php
+++ php/export.php
@@ -1,31 +1,45 @@
 <?php
-header('Content-Type: text/csv');
-header('Content-Disposition: attachment; filename="export.csv"');
-
+if (isset($_POST["export"])) {
+// Connect to the database
 $host = "localhost";
 $username = "root";
 $password = "";
 $dbname = "product db";
 $tablename = "product";
-if(isset($_POST["export"])) {
-    try {
-        $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $query = "SELECT code,label,type FROM $tablename";
-        $stmt = $conn->prepare($query);
-        $stmt->execute();
-        $result = $stmt->fetchAll();
-    
-        $file = fopen("php://output", "w");
-        foreach ($result as $row) {
-            fputcsv($file, $row);
-        }
-        fclose($file);
-        echo "Data exported successfully.";
-        
-    } catch(PDOException $e) {
-        echo "Error: " . $e->getMessage();
-    }
-    $conn = null;
-    }
+$conn = new mysqli($host, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Fetch the data from the table
+$query = "SELECT code,label,type FROM $tablename";
+$result = $conn->query($query);
+
+// Create a file pointer
+$fp = fopen("export.csv", "w");
+
+// Write the table headers to the CSV file
+fputcsv($fp, array("code", "label", "type"));
+
+// Write the table data to the CSV file
+while($row = $result->fetch_assoc()) {
+    fputcsv($fp, $row);
+}
+
+// Close the file pointer
+fclose($fp);
+
+// Download the CSV file
+header("Content-type: text/csv");
+header("Content-Disposition: attachment; filename=export.csv");
+header("Pragma: no-cache");
+header("Expires: 0");
+readfile("export.csv");
+
+// Close the database connection
+$conn->close();
+
+}
 ?>
